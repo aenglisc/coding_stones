fe3b8ea54038c91a6d8d1f7072b6940ee61fbf64
Roman Pushkov <pushkovroman@me.com>
2020-02-24 01:23:47 +0300

    add asymmetrical word square

--- exercises/word_square4.pl
+++ exercises/word_square4.pl
@@ -11,3 +11,22 @@ word_square(Words) :-
   word([W12, W22, W23, W24]),
   word([W13, W23, W33, W34]),
   word([W14, W24, W34, W44]).
+
+word_square_asymmetrical(Words) :-
+  Words = [
+    [ W11, W12, W13, W14 ],
+    [ W21, W22, W23, W24 ],
+    [ W31, W32, W33, W34 ],
+    [ W41, W42, W43, W44 ]
+  ],
+  word([W11, W12, W13, W14]),
+  word([W11, W21, W31, W41]),
+
+  word([W21, W22, W23, W24]),
+  word([W12, W22, W32, W42]),
+
+  word([W31, W32, W33, W34]),
+  word([W13, W23, W33, W43]),
+
+  word([W41, W42, W43, W44]),
+  word([W14, W24, W34, W44]).
